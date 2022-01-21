b0a499e95b417bd635320446986323134d65cbf6
Sharlotte Manganye <57158979+SharlotteManganye@users.noreply.github.com>
2022-01-21 17:08:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 sc = StandardScaler()
 
 X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
+X_test = sc.transform(X_test)
 
 n_input = X_train.shape[1]
 hidden_units = int(n_input)
